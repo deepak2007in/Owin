642bfe8e901b7737613afbc1b00ecfc125a01ebd
Deepak Agnihotri <deepak.agnihotri@9lenses.com>
2015-06-17 19:04:29 +0530

    Created the Javascript based unit test case

---
+++ JavascriptBasedUnitTesting/TestSuite.js
@@ -0,0 +1,17 @@
+﻿SlotMachine = function (buttonElement, balanceElement, reels, random, networkClient) {
+    this.buttonElement = buttonElement;
+    this.balanceElement = balanceElement;
+    this.reels = reels;
+    this.random = random;
+    this.networkClient = networkClient;
+    this.balance = 0;
+};
+
+SlotMachine.prototype.render = function () {
+    this.buttonElement.disabled = true;
+    this.buttonElement.value = 'Pay to play';
+    this.balanceElement.innerHTML = 0;
+    for (var i = 0; i < this.reels.length;) {
+        this.reels[i++].src = 'images/' + this.random() + '.jpg';
+    }
+};
