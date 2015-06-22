632f62885db3a023b546154d9f9def3220e500f3
Deepak Agnihotri <deepak.agnihotri@9lenses.com>
2015-06-22 13:52:26 +0530

    Added CasperJS related unit test cases

---
+++ JavascriptBasedUnitTesting/CasperSample.js
@@ -0,0 +1,10 @@
+﻿var casper = require('casper').create();
+casper.start('http://casperjs.org/', function(){
+    this.echo(this.getTitle());
+});
+
+casper.thenOpen('http://phantomjs.org', function () {
+    this.echo(this.getTitle());
+});
+
+casper.run();
