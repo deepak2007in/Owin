b8524f157dc4b0f5ae8c525bba624bd9d11b5106
Deepak Agnihotri <deepak.agnihotri@9lenses.com>
2015-07-06 17:03:40 +0530

    Client with the pre-configured REST service

--- ODataClient/Program.cs
+++ ODataClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,36 +11,47 @@ namespace ODataClient
     {
         static void Main(string[] args)
         {
-            string serviceUrl = "http://localhost:53687/";
-            var container = new Default.Container(new Uri(serviceUrl));
-            var product = new ODataClient.ODataWebAPI.Models.Product()
+            string serviceUrl = "http://services.odata.org/v4/(S(34wtn2c0hkuk5ekg0pjr513b))/TripPinServiceRW/";
+            var container = new DefaultContainer(new Uri(serviceUrl));
+
+            var deepak = new Person
             {
-                Id = 1,
-                Name = "Yo-Yo",
-                Category = "Toys",
-                Prize = 5.95M
+                UserName = "deepak.agnihotri1",
+                FirstName = "Deepak1",
+                LastName = "Agnihotri1",
+                Emails = new ObservableCollection<string> { "deepak.agnihotri1@9lenses.com" },
+                AddressInfo = new ObservableCollection<Location>
+                {
+                    new Location
+                    {
+                        Address = "204, Shapath",
+                        City = new City {CountryRegion = "India", Name = "Ahmedabad", Region = "Gujarat"}
+                    }
+                },
+                Gender = PersonGender.Male,
+                Concurrency = 635519729375200400
             };
 
-            // AddProduct(container: container, product: product);
+            AddPerson(container: container, person: deepak);
             ListAllProducts(container: container);
             Console.ReadLine();
         }
 
-        static void ListAllProducts(Default.Container container)
+        static void ListAllProducts(DefaultContainer container)
         {
-            foreach(var product in container.Products)
+            foreach(var person in container.People)
             {
-                Console.WriteLine("{0} {1} {2}", product.Name, product.Prize, product.Category);
+                Console.WriteLine("{0} {1}", person.FirstName, person.LastName);
             }
         }
 
-        static void AddProduct(Default.Container container, ODataClient.ODataWebAPI.Models.Product product)
+        static void AddPerson(DefaultContainer container, ODataClient.Person person)
         {
-            container.AddToProducts(product: product);
+            container.AddObject("People", person);
             var serviceResponse = container.SaveChanges();
-            foreach(var operationResponse in serviceResponse)
+            foreach (var operationResponse in serviceResponse)
             {
-                Console.WriteLine("Response: {0}", operationResponse.StatusCode);
+                Console.WriteLine("Response: Status Code {0}", operationResponse.StatusCode);
             }
         }
     }
