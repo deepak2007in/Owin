21722c26ed053f7216d78f1baf732a95dd423714
Deepak Agnihotri <deepak.agnihotri@9lenses.com>
2015-07-06 18:30:16 +0530

    RestAPI client using Sample OData

---
+++ RestClientWithSampleOData/Program.cs
@@ -0,0 +1,35 @@
+﻿using Simple.OData.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClientWithSampleOData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallCustomRest();
+            Console.ReadLine();
+        }
+
+        async static void CallBuiltInRest()
+        {
+            var x = ODataDynamic.Expression;
+            var client = new ODataClient("http://services.odata.org/v4/TripPinServiceRW/");
+            var people = await client
+                .For(x.People)
+                .Filter(x.Trips.Any(x.Budget > 3000))
+                .Top(2)
+                .Select(x.FirstName, x.LastName)
+                .FindEntriesAsync() as IEnumerable<dynamic>;
+
+            foreach(var person in people)
+            {
+                Console.WriteLine("{0} {1}", person.FirstName, person.LastName);
+            }
+        }
+    }
+}
